1aecd147c528094320d23b7c8c643786ed3964e9
Dominik Zogg <dominik.zogg@hotmail.com>
2021-10-10 11:02:01 +0200

    allow framework version 4.0

--- src/Router.php
+++ src/Router.php
@@ -2,130 +2,138 @@
 
 declare(strict_types=1);
 
-namespace Chubbyphp\Framework\Router\Sunrise;
-
-use Chubbyphp\Framework\Router\Exceptions\MethodNotAllowedException;
-use Chubbyphp\Framework\Router\Exceptions\MissingAttributeForPathGenerationException;
-use Chubbyphp\Framework\Router\Exceptions\MissingRouteByNameException;
-use Chubbyphp\Framework\Router\Exceptions\NotFoundException;
-use Chubbyphp\Framework\Router\Exceptions\NotMatchingValueForPathGenerationException;
-use Chubbyphp\Framework\Router\Route;
-use Chubbyphp\Framework\Router\RouteInterface;
-use Chubbyphp\Framework\Router\RouterInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Sunrise\Http\Router\Exception\InvalidAttributeValueException as SunriseInvalidAttributeValueException;
-use Sunrise\Http\Router\Exception\MethodNotAllowedException as SunriseMethodNotAllowedException;
-use Sunrise\Http\Router\Exception\MissingAttributeValueException as SunriseMissingAttributeValueException;
-use Sunrise\Http\Router\Exception\RouteNotFoundException as SunriseRouteNotFoundException;
-use Sunrise\Http\Router\RouteFactory;
-use Sunrise\Http\Router\Router as SunriseRouter;
-
-final class Router implements RouterInterface
-{
-    private SunriseRouter $router;
-
-    private string $basePath;
-
-    /**
-     * @param array<int, RouteInterface> $routes
-     */
-    public function __construct(array $routes, string $basePath = '')
-    {
-        $this->router = $this->createRouter($routes);
-        $this->basePath = $basePath;
-    }
-
-    public function match(ServerRequestInterface $request): RouteInterface
-    {
-        try {
-            $sunriseRoute = $this->router->match($request);
-
-            return Route::create(
-                $request->getMethod(),
-                $sunriseRoute->getPath(),
-                $sunriseRoute->getName(),
-                $sunriseRoute->getRequestHandler(),
-                $sunriseRoute->getMiddlewares()
-            )->withAttributes($sunriseRoute->getAttributes());
-        } catch (SunriseRouteNotFoundException $exception) {
-            throw NotFoundException::create($request->getRequestTarget());
-        } catch (SunriseMethodNotAllowedException $exception) {
-            throw MethodNotAllowedException::create(
-                $request->getRequestTarget(),
-                $request->getMethod(),
-                $exception->getAllowedMethods()
-            );
+namespace Chubbyphp\Framework\Router {
+    if (!interface_exists('RouteInterface')) {
+        interface RouterInterface extends RouteMatcherInterface, UrlGeneratorInterface
+        {
         }
     }
+}
 
-    /**
-     * @param array<string, string> $attributes
-     * @param array<string, mixed>  $queryParams
-     */
-    public function generateUrl(
-        ServerRequestInterface $request,
-        string $name,
-        array $attributes = [],
-        array $queryParams = []
-    ): string {
-        $uri = $request->getUri();
-        $requestTarget = $this->generatePath($name, $attributes, $queryParams);
+namespace Chubbyphp\Framework\Router\Sunrise {
+    use Chubbyphp\Framework\Router\Exceptions\MethodNotAllowedException;
+    use Chubbyphp\Framework\Router\Exceptions\MissingAttributeForPathGenerationException;
+    use Chubbyphp\Framework\Router\Exceptions\MissingRouteByNameException;
+    use Chubbyphp\Framework\Router\Exceptions\NotFoundException;
+    use Chubbyphp\Framework\Router\Exceptions\NotMatchingValueForPathGenerationException;
+    use Chubbyphp\Framework\Router\Route;
+    use Chubbyphp\Framework\Router\RouteInterface;
+    use Chubbyphp\Framework\Router\RouterInterface;
+    use Psr\Http\Message\ServerRequestInterface;
+    use Sunrise\Http\Router\Exception\InvalidAttributeValueException as SunriseInvalidAttributeValueException;
+    use Sunrise\Http\Router\Exception\MethodNotAllowedException as SunriseMethodNotAllowedException;
+    use Sunrise\Http\Router\Exception\MissingAttributeValueException as SunriseMissingAttributeValueException;
+    use Sunrise\Http\Router\Exception\RouteNotFoundException as SunriseRouteNotFoundException;
+    use Sunrise\Http\Router\RouteFactory;
+    use Sunrise\Http\Router\Router as SunriseRouter;
 
-        return $uri->getScheme().'://'.$uri->getAuthority().$requestTarget;
-    }
-
-    /**
-     * @param array<string, string> $attributes
-     * @param array<string, mixed>  $queryParams
-     */
-    public function generatePath(string $name, array $attributes = [], array $queryParams = []): string
+    final class Router implements RouterInterface
     {
-        try {
-            $path = $this->router->generateUri($name, $attributes, true);
-        } catch (SunriseRouteNotFoundException $exception) {
-            throw MissingRouteByNameException::create($name);
-        } catch (SunriseMissingAttributeValueException $exception) {
-            $match = $exception->fromContext('match');
+        private SunriseRouter $router;
 
-            throw MissingAttributeForPathGenerationException::create($name, $match['name']);
-        } catch (SunriseInvalidAttributeValueException $exception) {
-            $match = $exception->fromContext('match');
-            $value = $exception->fromContext('value');
+        private string $basePath;
 
-            throw NotMatchingValueForPathGenerationException::create(
-                $name,
-                $match['name'],
-                $value,
-                $match['pattern']
-            );
+        /**
+         * @param array<int, RouteInterface> $routes
+         */
+        public function __construct(array $routes, string $basePath = '')
+        {
+            $this->router = $this->createRouter($routes);
+            $this->basePath = $basePath;
         }
 
-        if ([] === $queryParams) {
-            return $this->basePath.$path;
+        public function match(ServerRequestInterface $request): RouteInterface
+        {
+            try {
+                $sunriseRoute = $this->router->match($request);
+
+                return Route::create(
+                    $request->getMethod(),
+                    $sunriseRoute->getPath(),
+                    $sunriseRoute->getName(),
+                    $sunriseRoute->getRequestHandler(),
+                    $sunriseRoute->getMiddlewares()
+                )->withAttributes($sunriseRoute->getAttributes());
+            } catch (SunriseRouteNotFoundException $exception) {
+                throw NotFoundException::create($request->getRequestTarget());
+            } catch (SunriseMethodNotAllowedException $exception) {
+                throw MethodNotAllowedException::create(
+                    $request->getRequestTarget(),
+                    $request->getMethod(),
+                    $exception->getAllowedMethods()
+                );
+            }
         }
 
-        return $this->basePath.$path.'?'.http_build_query($queryParams);
-    }
+        /**
+         * @param array<string, string> $attributes
+         * @param array<string, mixed>  $queryParams
+         */
+        public function generateUrl(
+            ServerRequestInterface $request,
+            string $name,
+            array $attributes = [],
+            array $queryParams = []
+        ): string {
+            $uri = $request->getUri();
+            $requestTarget = $this->generatePath($name, $attributes, $queryParams);
 
-    /**
-     * @param array<RouteInterface> $routes
-     */
-    private function createRouter(array $routes): SunriseRouter
-    {
-        $routeFactory = new RouteFactory();
-        $router = new SunriseRouter();
-
-        foreach ($routes as $route) {
-            $router->addRoute($routeFactory->createRoute(
-                $route->getName(),
-                $route->getPath(),
-                [$route->getMethod()],
-                $route->getRequestHandler(),
-                $route->getMiddlewares(),
-                $route->getAttributes()
-            ));
+            return $uri->getScheme().'://'.$uri->getAuthority().$requestTarget;
         }
 
-        return $router;
+        /**
+         * @param array<string, string> $attributes
+         * @param array<string, mixed>  $queryParams
+         */
+        public function generatePath(string $name, array $attributes = [], array $queryParams = []): string
+        {
+            try {
+                $path = $this->router->generateUri($name, $attributes, true);
+            } catch (SunriseRouteNotFoundException $exception) {
+                throw MissingRouteByNameException::create($name);
+            } catch (SunriseMissingAttributeValueException $exception) {
+                $match = $exception->fromContext('match');
+
+                throw MissingAttributeForPathGenerationException::create($name, $match['name']);
+            } catch (SunriseInvalidAttributeValueException $exception) {
+                $match = $exception->fromContext('match');
+                $value = $exception->fromContext('value');
+
+                throw NotMatchingValueForPathGenerationException::create(
+                    $name,
+                    $match['name'],
+                    $value,
+                    $match['pattern']
+                );
+            }
+
+            if ([] === $queryParams) {
+                return $this->basePath.$path;
+            }
+
+            return $this->basePath.$path.'?'.http_build_query($queryParams);
+        }
+
+        /**
+         * @param array<RouteInterface> $routes
+         */
+        private function createRouter(array $routes): SunriseRouter
+        {
+            $routeFactory = new RouteFactory();
+            $router = new SunriseRouter();
+
+            foreach ($routes as $route) {
+                $router->addRoute($routeFactory->createRoute(
+                    $route->getName(),
+                    $route->getPath(),
+                    [$route->getMethod()],
+                    $route->getRequestHandler(),
+                    $route->getMiddlewares(),
+                    $route->getAttributes()
+                ));
+            }
+
+            return $router;
+        }
     }
 }
